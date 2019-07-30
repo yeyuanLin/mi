0d569a63debb498a12476d79d439eb9e72a76026
yeyuanLin <15797731292@163.com>
2019-07-30 21:09:50 +0800

    购物车部分

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
-// 1.查找触发事件的元素
-$(".topbar-cart").hover(function(){
-    $(".cart-mini").addClass(".cart-mini-active")
-    $(".cart-menu").fadeToggle();
-    $(".loading").text("购物车还没有商品，赶紧选购吧！");
-
-})
+// // 1.查找触发事件的元素
+// $(".topbar-cart").hover(function(){
+//     $(".cart-mini").addClass(".cart-mini-active")
+//     $(".cart-menu").fadeToggle();
+//     $(".loading").text("购物车还没有商品，赶紧选购吧！");
+//
+// })
